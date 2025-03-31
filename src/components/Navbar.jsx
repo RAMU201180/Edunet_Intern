9beb3478034b2aae5f343796509727179a69a6fb
RAMU201180 <165267181+RAMU201180@users.noreply.github.com>
2025-03-31 21:03:14 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Ensure this import is present
+import { Link } from "react-router-dom"; 
 
 function NavigationBar() {
   return (
